b8b6f3e231b4a9dfbc04adf1c98989c9bb233dad
Vikram S <vikram.senthil@outlook.com>
2020-11-02 10:23:32 +0530

    Inital Commint

---
+++ src/main/java/com/vikram/springboot/PMSRESTAPI/PmsRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.vikram.springboot.PMSRESTAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmsRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmsRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vikram/springboot/PMSRESTAPI/PmsRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikram.springboot.PMSRESTAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmsRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
