f69faa2760d57e48b17da5bb03dd11231bde782e
Vikram S <vikram.senthil@outlook.com>
2020-11-04 22:49:32 +0530

    Create ProjectRepository.java

---
+++ src/main/java/com/vikram/springboot/PMSRESTAPI/repository/ProjectRepository.java
@@ -0,0 +1,11 @@
+package com.vikram.springboot.PMSRESTAPI.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vikram.springboot.PMSRESTAPI.model.Project;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project,Long>{
+
+}
